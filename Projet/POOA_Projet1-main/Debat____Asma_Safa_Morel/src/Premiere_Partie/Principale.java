001d3340ed31baed9053e2a1b244815bdfe4c98b
tresor225 <117073776+tresor225@users.noreply.github.com>
2022-10-31 12:06:09 +0100

    Add files via upload

---
+++ Projet/POOA_Projet1-main/Debat____Asma_Safa_Morel/src/Premiere_Partie/Principale.java
@@ -0,0 +1,13 @@
+package Premiere_Partie;
+
+public class Principale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Menu menu= new Menu();
+		//menu.premierMenu();
+		menu.deuxiemeMenu(menu.premierMenu());
+	}
+
+}
